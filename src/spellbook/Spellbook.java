1a520da86a7b8e457d7c8a4126b93f9f68489196
Yawrf <rewilloughby@ymail.com>
2019-04-10 21:19:15 -0400

    Prep for Parent App
    A minor tweak to integrate it into the Parent App

--- src/spellbook/Spellbook.java
+++ src/spellbook/Spellbook.java
@@ -17,15 +17,21 @@ import javafx.stage.Stage;
  */
 public class Spellbook extends Application {
     
+    private static Scene scene = null;
+    
     @Override
     public void start(Stage stage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
-        Scene scene = new Scene(root);
+        scene = new Scene(root);
         
         stage.setTitle("Spellbook by Yawrf");
         stage.setScene(scene);
         stage.show();
+    }
+    
+    public static Scene getScene() {
+        return scene;
     }
 
     /**
